171e8c8eabdf937214fa03ef9c446f626a564d51
Kamil Jarzab <bogurodzic@gmail.com>
2017-07-10 22:45:18 +0200

    Installed google maps api via npm

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,19 @@
 var $ = require("jquery");
 var ko = require("knockout");
+
+
+console.log($);
+
+var GoogleMapsLoader = require('google-maps'); // only for common js environments
+
+GoogleMapsLoader.load(function(google) {
+    new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: {lat: -33, lng: 151},
+          mapTypeControl: true,
+          mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            mapTypeIds: ['roadmap', 'terrain']
+          }
+        });
+});
