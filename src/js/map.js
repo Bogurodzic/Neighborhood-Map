4873cadf997ea9e841e537812dfc2b1780e7c96c
Kamil Jarzab <bogurodzic@gmail.com>
2017-07-28 14:12:14 +0200

    Added closeAllMarkers method

--- src/js/map.js
+++ src/js/map.js
@@ -12,13 +12,34 @@ GoogleMapsLoader.load(function(google) {
 
   let allPlaces = viewModel.allPlaces();
 
+  function closeAllMarkers(){
+    viewModel.markers().forEach(function(marker){
+      marker.infoWindow.close(map, marker);
+    })
+  }
+
   allPlaces.forEach(function(place, index){
     let marker = new google.maps.Marker({
       position: {lat: place.lat, lng: place.lng},
       map: viewModel.map,
       title: place.name
     });
+    //Create new infowindow
+    let infoWindow = new google.maps.InfoWindow({
+        content: ":DDDDD"
+    });
+
+    marker.infoWindow = infoWindow;
+    //Add info window to click event
+    marker.addListener('click', function() {
+      closeAllMarkers();
+      marker.infoWindow.open(map, marker);
+    });
+    //Add marker to markers array
     viewModel.markers.push(marker);
-  })
+  }),
+
+
+
   console.log(viewModel.markers())
 });
