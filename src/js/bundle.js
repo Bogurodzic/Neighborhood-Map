bdad4bab5caa2585f6b8c820f31c7e4e4b920298
Kamil Jarzab <bogurodzic@gmail.com>
2017-07-28 14:33:24 +0200

    Code refactorisation

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -10343,43 +10343,66 @@ return jQuery;
 var GoogleMapsLoader = __webpack_require__(3);
 var viewModel = __webpack_require__(4);
 
+initMap();
+
 GoogleMapsLoader.KEY = 'AIzaSyBwTkrCtLKEQD5ocyIcgNZgCwQFjwtMRs0';
 
-GoogleMapsLoader.load(function (google) {
-  viewModel.map = new google.maps.Map(document.getElementById('map'), {
+function initMap() {
+  GoogleMapsLoader.load(function (google) {
+    renderMap();
+    createAllMarkers(getAllPlaces());
+
+    console.log(viewModel.markers());
+  });
+}
+
+function renderMap() {
+  return viewModel.map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 52.231838, lng: 21.0038063 },
     zoom: 13
   });
+}
 
-  var allPlaces = viewModel.allPlaces();
+function getAllPlaces() {
+  return viewModel.allPlaces();
+}
 
-  function closeAllMarkers() {
-    viewModel.markers().forEach(function (marker) {
-      marker.infoWindow.close(map, marker);
-    });
-  }
-
-  allPlaces.forEach(function (place, index) {
-    var marker = new google.maps.Marker({
-      position: { lat: place.lat, lng: place.lng },
-      map: viewModel.map,
-      title: place.name
-    });
-    //Create new infowindow
-    var infoWindow = new google.maps.InfoWindow({
-      content: ":DDDDD"
-    });
-
-    marker.infoWindow = infoWindow;
-    //Add info window to click event
-    marker.addListener('click', function () {
-      closeAllMarkers();
-      marker.infoWindow.open(map, marker);
-    });
+function createAllMarkers(allMarkers) {
+  allMarkers.forEach(function (place) {
+    var marker = createMarker(place);
+    marker.infoWindow = createInfoWindow();
+    addListeners(marker);
     //Add marker to markers array
     viewModel.markers.push(marker);
-  }), console.log(viewModel.markers());
-});
+  });
+}
+
+function createMarker(place) {
+  return new google.maps.Marker({
+    position: { lat: place.lat, lng: place.lng },
+    map: viewModel.map,
+    title: place.name
+  });
+}
+
+function createInfoWindow() {
+  return new google.maps.InfoWindow({
+    content: ":DDDDD"
+  });
+}
+
+function addListeners(marker) {
+  marker.addListener('click', function () {
+    closeAllMarkers();
+    marker.infoWindow.open(map, marker);
+  });
+}
+
+function closeAllMarkers() {
+  viewModel.markers().forEach(function (marker) {
+    marker.infoWindow.close(map, marker);
+  });
+}
 
 /***/ }),
 /* 3 */
--- src/js/map.js
+++ src/js/map.js
@@ -2,44 +2,61 @@ const GoogleMapsLoader = require('google-maps');
 let viewModel = require("./viewModel.js");
 
 
+initMap();
+
 GoogleMapsLoader.KEY = 'AIzaSyBwTkrCtLKEQD5ocyIcgNZgCwQFjwtMRs0';
 
-GoogleMapsLoader.load(function(google) {
-  viewModel.map = new google.maps.Map(document.getElementById('map'), {
+function initMap(){
+  GoogleMapsLoader.load(function(google) {
+    renderMap();
+    createAllMarkers(getAllPlaces());
+  });
+}
+
+function renderMap(){
+  return viewModel.map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 52.231838, lng: 21.0038063},
         zoom: 13
   });
+}
 
-  let allPlaces = viewModel.allPlaces();
+function getAllPlaces(){
+  return viewModel.allPlaces()
+}
 
-  function closeAllMarkers(){
-    viewModel.markers().forEach(function(marker){
-      marker.infoWindow.close(map, marker);
-    })
-  }
-
-  allPlaces.forEach(function(place, index){
-    let marker = new google.maps.Marker({
-      position: {lat: place.lat, lng: place.lng},
-      map: viewModel.map,
-      title: place.name
-    });
-    //Create new infowindow
-    let infoWindow = new google.maps.InfoWindow({
-        content: ":DDDDD"
-    });
-
-    marker.infoWindow = infoWindow;
-    //Add info window to click event
-    marker.addListener('click', function() {
-      closeAllMarkers();
-      marker.infoWindow.open(map, marker);
-    });
+function createAllMarkers(allMarkers){
+  allMarkers.forEach(function(place){
+    let marker = createMarker(place);
+    marker.infoWindow = createInfoWindow();
+    addListeners(marker);
     //Add marker to markers array
     viewModel.markers.push(marker);
-  }),
+  })
+}
 
+function createMarker(place){
+  return new google.maps.Marker({
+        position: {lat: place.lat, lng: place.lng},
+        map: viewModel.map,
+        title: place.name
+      });
+}
 
+function createInfoWindow(){
+  return new google.maps.InfoWindow({
+      content: ":DDDDD"
+  });
+}
 
-  console.log(viewModel.markers())
-});
+function addListeners(marker){
+  marker.addListener('click', function() {
+    closeAllMarkers();
+    marker.infoWindow.open(map, marker);
+  });
+}
+
+function closeAllMarkers(){
+  viewModel.markers().forEach(function(marker){
+    marker.infoWindow.close(map, marker);
+  })
+}
